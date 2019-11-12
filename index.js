753a6087a8833f59e5d0b4eef34803e1bbf0c1ec
Phong Yang <phongyang88@hotmail.com>
2019-11-12 16:23:19 -0600

    skipped and commented out stretch challenge. finished getFullNames

--- index.js
+++ index.js
@@ -147,28 +147,28 @@ function processContains(item, list, callback) {
   return callback(answer);
 }
 
-/**
- * ### Challenge `processDuplicateFree`
- * THIS IS A STRETCH PROBLEM! ATTEMPT ONLY AFTER COMPLETING ALL NON-STRETCH CHALLENGES!
- * THIS IS A STRETCH PROBLEM! ATTEMPT ONLY AFTER COMPLETING ALL NON-STRETCH CHALLENGES!
- * 
- * @instructions
- * Implement a higher-order function called `processDuplicateFree`.
- * It takes two arguments:
- * @param list array of elements of any kind.
- * @param callback function that takes an array as its argument.
- * @returns the result of invoking `callback` passing a de-duped version of `list`.
- * 
- * Examples of usage of this higher-order function:
- * [1] Invoking `processDuplicateFree` passing `[1,1,1,2]` and `(arr) => arr`,
- * should return `[1,2]`.
- * 
- * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
- * should return 3.
-*/
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
-}
+// /**
+//  * ### Challenge `processDuplicateFree`
+//  * THIS IS A STRETCH PROBLEM! ATTEMPT ONLY AFTER COMPLETING ALL NON-STRETCH CHALLENGES!
+//  * THIS IS A STRETCH PROBLEM! ATTEMPT ONLY AFTER COMPLETING ALL NON-STRETCH CHALLENGES!
+//  * 
+//  * @instructions
+//  * Implement a higher-order function called `processDuplicateFree`.
+//  * It takes two arguments:
+//  * @param list array of elements of any kind.
+//  * @param callback function that takes an array as its argument.
+//  * @returns the result of invoking `callback` passing a de-duped version of `list`.
+//  * 
+//  * Examples of usage of this higher-order function:
+//  * [1] Invoking `processDuplicateFree` passing `[1,1,1,2]` and `(arr) => arr`,
+//  * should return `[1,2]`.
+//  * 
+//  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
+//  * should return 3.
+// */
+// function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
+//   /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+// }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
@@ -188,8 +188,12 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let newArray = [];
+  runners.forEach(function(element){
+    newArray.push(`${element.last_name}, ${element.first_name}`)
+  })
+  return newArray;
 }
 
 /**
@@ -204,8 +208,11 @@ function getFullNames(/* CODE HERE */) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  let newArray = runners.map(function(element){
+    return element.first_name.toUpperCase();
+  })
+  return newArray;
 }
 
 /**
